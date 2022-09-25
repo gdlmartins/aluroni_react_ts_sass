f2bc824c2246b16e5fd5c99a11f58dcc105fa54c
Gabriel Martins <87931081+gdlmartins@users.noreply.github.com>
2022-09-25 22:39:56 +0800

    feature and styling

---
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -0,0 +1,9 @@
+export const  Buscador =()=>{
+
+    return(
+        <div>
+        Buscador
+        </div>
+    )
+}
+export default Buscador;
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,10 +1,26 @@
 import styles from "./Cardapio.module.scss";
-
+import {ReactComponent as Logo} from "assets/logo.svg";
+import Buscador from "./Buscador";
 export const Cardapio =() =>{
 
     return(
         <main>
-            <nav className="menu"></nav>
+            <nav className={styles.menu}>
+                <Logo/>
+            </nav>
+            <header className={styles.header}>
+                <div className={styles.header__text}>
+                    A casa do codigo de da massa
+
+                </div>
+
+            </header>
+            <section className={styles.cardapio}>
+                <h3 className={styles.cardapio__titulo}>
+                    Cardapio
+                </h3>
+                <Buscador/>
+            </section>
         </main>
     )
 
